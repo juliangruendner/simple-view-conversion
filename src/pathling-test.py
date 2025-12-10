7ea3d0ee1bd8047778b8951f0c6afa71d78684b6
Julian Gründner <juliangruendner@googlemail.com>
2025-12-10 11:17:37 +0100

    add reading in of view definitions directly

--- src/pathling-test.py
+++ src/pathling-test.py
@@ -1,13 +1,28 @@
 from pathling import PathlingContext
+import json
+
+
+def get_view_def(file_path):
+
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+def run_view_def(view_def_name):
+    view_def = get_view_def(f"../viewDefinitions/{view_def_name}")
+
+    view_def_resource = view_def["resource"]["resource"]
+    view_def_select = view_def["resource"]["select"]
+
+    result = data.view(
+        resource=view_def_resource,
+        select=view_def_select,
+    )
+
+    result.show(truncate=False)
+
 
 pc = PathlingContext.create()
 data = pc.read.ndjson("../input")
-
-
-result = data.view(
-    resource="Patient",
-    select=[
-        {"column": [{"path": "id", "name": "id"}]}
-    ],
-)
-result.show(truncate=False)
+run_view_def("pat-view-def.json")
+run_view_def("cond-view-def.json")
